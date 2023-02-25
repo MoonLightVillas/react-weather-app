c50e1d702f66a1453c556966d5fc521397f78f43
Mariela Campos <102127440+MoonLightVillas@users.noreply.github.com>
2023-02-24 16:52:51 -0800

    Added files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
-}
+  return (
+  <div className="App">
+    <div className="container">
+      <Weather defaultCity="New York" />
+      <footer>
+          This project was coded by{" "}
+          <a
+            href="https://marielacampos.slack.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mariela Campos
+          </a>{" "}
+          and is{" "}
+          <a
+          href="https://github.com/MoonLightVillas/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer">
+            hosted on Netlify
+          </a>
+      </footer>
+    </div>
+  </div>
+  );
+  }  
